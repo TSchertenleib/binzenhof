3ec0e19e0017b8af327c20c34595cea081c0809f
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-07-27 16:57:52 +0200

    neuer upload
    grosser upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,81 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+
 $(document).ready(function ()	{
-	
 
-    $('.unterliste').hover(function() {
-        $(this).addClass('transition');
- 
-    }, function() {
-        $(this).removeClass('transition');
-    });
+    $(".uebertitel").css("display","none");
 
-	$('.button').hover(function() {
-        $(this).addClass('transition');
- 
-    }, function() {
-        $(this).removeClass('transition');
-    });
-    
+
     $('.button').hover(function() {
-	    $('#navigation').addClass('active');
+        $('.inhalt').addClass('start-effect');
+        $('.bgimage img').addClass('img-zoom');
+        $('.uebertitel').fadeIn('slow');
+
     }, function() {
-        $('#navigation').removeClass('active');
+        $('.inhalt').removeClass('start-effect');
+        $('.bgimage img').removeClass('img-zoom');
+        $(".uebertitel").fadeOut('slow');
+
     });
-    
+
+    $('.button').click(function() {
+
+        $.fn.scrollView = function () {
+            return this.each(function () {
+                $('html, body').animate({
+                    scrollTop: $(this).offset().top
+                }, 1000);
+            });
+        }
+        $('.main-navigation').scrollView();
+    });
+
+$('.button').blurjs({
+    source: 'body',
+    radius: 10
+});
+
 
 });
 
