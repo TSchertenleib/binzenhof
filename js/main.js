522f010af7743cfe23c2eb530fe9c2b848e38a44
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-08-10 16:03:02 +0200

    slick slider working

--- js/main.js
+++ js/main.js
@@ -169,10 +169,18 @@ $('.button').hover(function() {
       dots: true,
       infinite: false,
       speed: 300,
-      slidesToShow: 1,
+      slidesToShow: 3,
       slidesToScroll: 3,
       responsive: [
-
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
       {
         breakpoint: 667,
         settings: {
