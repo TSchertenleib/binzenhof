ef88e97a001e90fab581a56ac77e845e411906b4
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-08-10 20:15:11 +0200

    beide arrows sichtbar

--- js/main.js
+++ js/main.js
@@ -162,44 +162,55 @@ $('.button').hover(function() {
     });
 
 
-    // slider
+    // slick slider
 
     $('.kuehe-slider').slick({
-      arrows: true,
       dots: true,
-      infinite: false,
+      infinite: true,
       speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 667,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ]
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+
+      // centerMode: true,
+      // centerPadding: '10vw',
+
+    //   responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 667,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // // You can unslick at a given breakpoint now by adding:
+    // // settings: "unslick"
+    // // instead of a settings object
+    // ]
   });
+    
+    $('.next-button-slick').click(function(){
+        $('.kuehe-slider').slickNext();
+    });
+    $('.prev-button-slick').click(function(){
+        $('.kuehe-slider').slickPrev();
+    });
 
 
     function initialize() {
